5279db535cb6c1a9c36c6bca78e71f68c57d3c5b
Paul <pauloselmatzidis2006@gmail.com>
2026-05-03 12:41:00 +0300

    Updates

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import "./style.css";
-import { Task } from "./modules/Task";
-import { setLocalStorage,getStorage } from "./modules/Storage";
-import { showForm, submitTask, displayTasks, createForm,elementBuilder,initApp } from "./modules/DOM";
-
-initApp();
--- src/modules/Task.js
+++
@@ -1,9 +0,0 @@
-//BUIDLIG TASK: this is the structure for each task there is in the page
-export class Task {
-  constructor(title, description, duedate, priority) {
-    this.title = title;
-    this.description = description;
-    this.duedate = duedate;
-    this.priority = priority;
-  }
-}
